211a63483d9d44f32e31f3d876e0477dd712e12c
heshanu3d <heshann@foxmail.com>
2023-01-25 20:56:53 +0800

    decorate list_view
     - using table

--- ui.py
+++ ui.py
@@ -2,126 +2,194 @@
 
 import os, re
 from xmlrpc.client import Boolean
-import dearpygui.dearpygui as ui
+import dearpygui.dearpygui as dpg
 import dearpygui.demo as demo
 
+def _config(sender, keyword, user_data):
 
-def set_theme(show_debug_editor : Boolean):
-    with ui.theme() as global_theme:
+    widget_type = dpg.get_item_type(sender)
+    items = user_data
 
-        # with ui.theme_component(ui.mvAll):
-        #     ui.add_theme_color(ui.mvThemeCol_FrameBg, (255, 140, 23), category=ui.mvThemeCat_Core)
-        #     ui.add_theme_style(ui.mvStyleVar_FrameRounding, 5, category=ui.mvThemeCat_Core)
+    if widget_type == "mvAppItemType::mvRadioButton":
+        value = True
 
-        # with ui.theme_component(ui.mvInputInt):
-        #     ui.add_theme_color(ui.mvThemeCol_FrameBg, (140, 255, 23), category=ui.mvThemeCat_Core)
-        #     ui.add_theme_style(ui.mvStyleVar_FrameRounding, 5, category=ui.mvThemeCat_Core)
-        with ui.theme_component(ui.mvAll):
-            ui.add_theme_color(ui.mvThemeCol_FrameBg, (226, 226, 226), category=ui.mvThemeCat_Core)
-            ui.add_theme_style(ui.mvStyleVar_FrameBorderSize, 1, category=ui.mvThemeCat_Core)
-            ui.add_theme_style(ui.mvStyleVar_FrameRounding, 10, category=ui.mvThemeCat_Core)
+    else:
+        keyword = dpg.get_item_label(sender)
+        value = dpg.get_value(sender)
 
-            ui.add_theme_color(ui.mvThemeCol_Text, (0, 0, 0, 255))
-            ui.add_theme_color(ui.mvThemeCol_ChildBg, (226, 226, 226, 255))
-            ui.add_theme_color(ui.mvThemeCol_WindowBg, (226, 226, 226, 255))
-            ui.add_theme_color(ui.mvThemeCol_FrameBg, (226, 226, 226, 255))
-            ui.add_theme_color(ui.mvThemeCol_MenuBarBg, (226, 226, 226, 255))
-            ui.add_theme_color(ui.mvThemeCol_Header, (226, 226, 226, 255))
+    if isinstance(user_data, list):
+        for item in items:
+            dpg.configure_item(item, **{keyword: value})
+    else:
+        dpg.configure_item(items, **{keyword: value})
 
-            ui.add_theme_color(ui.mvThemeCol_ScrollbarBg, (226, 226, 226, 255))
-            ui.add_theme_color(ui.mvThemeCol_ScrollbarGrab, (200, 200, 200, 255))
-            ui.add_theme_color(ui.mvThemeCol_ScrollbarGrabActive, (140, 140, 140, 140))
-            ui.add_theme_color(ui.mvThemeCol_ScrollbarGrabHovered, (170, 170, 170, 255))
+def _add_config_options(item, columns, *names, **kwargs):
 
-            ui.add_theme_color(ui.mvThemeCol_Tab, (226, 226, 226, 255))
-            ui.add_theme_color(ui.mvThemeCol_TableHeaderBg, (226, 226, 226, 255))
-            ui.add_theme_color(ui.mvThemeCol_TabHovered, (198, 232, 255, 255))
-            ui.add_theme_color(ui.mvThemeCol_TabActive, (153, 209, 255, 255))
+    if columns == 1:
+        if 'before' in kwargs:
+            for name in names:
+                dpg.add_checkbox(label=name, callback=_config, user_data=item, before=kwargs['before'], default_value=dpg.get_item_configuration(item)[name])
+        else:
+            for name in names:
+                dpg.add_checkbox(label=name, callback=_config, user_data=item, default_value=dpg.get_item_configuration(item)[name])
 
+    else:
 
-    ui.bind_theme(global_theme)
+        if 'before' in kwargs:
+            dpg.push_container_stack(dpg.add_table(header_row=False, before=kwargs['before']))
+        else:
+            dpg.push_container_stack(dpg.add_table(header_row=False))
 
-    if show_debug_editor:
-        ui.show_style_editor()
+        for i in range(columns):
+            dpg.add_table_column()
+
+        for i in range(int(len(names)/columns)):
+
+            with dpg.table_row():
+                for j in range(columns):
+                    dpg.add_checkbox(label=names[i*columns + j],
+                                     callback=_config, user_data=item,
+                                     default_value=dpg.get_item_configuration(item)[names[i*columns + j]])
+        dpg.pop_container_stack()
+
+def show_debug_editor(is_show : Boolean):
+    if is_show:
+        dpg.show_style_editor()
         demo.show_demo()
-        ui.show_font_manager()
+        dpg.show_font_manager()
         return 1395
     else:
-        return 410
+        return 630
+
+def set_theme(themes: list):
+    with dpg.theme() as border_theme:
+        with dpg.theme_component(dpg.mvAll):
+            dpg.add_theme_style(dpg.mvStyleVar_FrameBorderSize, 1, category=dpg.mvThemeCat_Core)
+            dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 10, category=dpg.mvThemeCat_Core)
+
+    with dpg.theme() as global_theme:
+
+        # with dpg.theme_component(dpg.mvAll):
+        #     dpg.add_theme_color(dpg.mvThemeCol_FrameBg, (255, 140, 23), category=dpg.mvThemeCat_Core)
+        #     dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 5, category=dpg.mvThemeCat_Core)
+
+        # with dpg.theme_component(dpg.mvInputInt):
+        #     dpg.add_theme_color(dpg.mvThemeCol_FrameBg, (140, 255, 23), category=dpg.mvThemeCat_Core)
+        #     dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 5, category=dpg.mvThemeCat_Core)
+        with dpg.theme_component(dpg.mvAll):
+
+            dpg.add_theme_style(dpg.mvStyleVar_FrameBorderSize, 0, category=dpg.mvThemeCat_Core)
+            dpg.add_theme_style(dpg.mvStyleVar_FrameRounding, 0, category=dpg.mvThemeCat_Core)
+
+            dpg.add_theme_style(dpg.mvStyleVar_WindowPadding, 0, category=dpg.mvThemeCat_Core)
+            dpg.add_theme_style(dpg.mvStyleVar_FramePadding, 0, category=dpg.mvThemeCat_Core)
+            dpg.add_theme_style(dpg.mvStyleVar_CellPadding, 0, category=dpg.mvThemeCat_Core)
+            dpg.add_theme_style(dpg.mvStyleVar_ItemSpacing, 0, category=dpg.mvThemeCat_Core)
+
+            dpg.add_theme_color(dpg.mvThemeCol_FrameBg, (226, 226, 226), category=dpg.mvThemeCat_Core)
+            dpg.add_theme_color(dpg.mvThemeCol_Text, (0, 0, 0, 255))
+            dpg.add_theme_color(dpg.mvThemeCol_ChildBg, (226, 226, 226, 255))
+            dpg.add_theme_color(dpg.mvThemeCol_WindowBg, (226, 226, 226, 255))
+            dpg.add_theme_color(dpg.mvThemeCol_FrameBg, (226, 226, 226, 255))
+            dpg.add_theme_color(dpg.mvThemeCol_MenuBarBg, (226, 226, 226, 255))
+            dpg.add_theme_color(dpg.mvThemeCol_Header, (226, 226, 226, 255))
+
+            dpg.add_theme_color(dpg.mvThemeCol_Button, (226, 226, 226, 255))
+
+            dpg.add_theme_color(dpg.mvThemeCol_ScrollbarBg, (226, 226, 226, 255))
+            dpg.add_theme_color(dpg.mvThemeCol_ScrollbarGrab, (200, 200, 200, 255))
+            dpg.add_theme_color(dpg.mvThemeCol_ScrollbarGrabActive, (140, 140, 140, 140))
+            dpg.add_theme_color(dpg.mvThemeCol_ScrollbarGrabHovered, (170, 170, 170, 255))
+
+            dpg.add_theme_color(dpg.mvThemeCol_Tab, (226, 226, 226, 255))
+            dpg.add_theme_color(dpg.mvThemeCol_TableHeaderBg, (226, 226, 226, 255))
+            dpg.add_theme_color(dpg.mvThemeCol_TabHovered, (198, 232, 255, 255))
+            dpg.add_theme_color(dpg.mvThemeCol_TabActive, (153, 209, 255, 255))
+
+    dpg.bind_theme(global_theme)
+
+    themes.append(global_theme)
+    themes.append(border_theme)
 
 def set_font(debug_mode : Boolean):
     size = 20
-    with ui.font_registry():
-        with ui.font('NotoSansSC-Regular.otf', size) as simple_chinese_font:
-            ui.add_font_range_hint(ui.mvFontRangeHint_Default)
-            ui.add_font_range_hint(ui.mvFontRangeHint_Chinese_Simplified_Common)
+    with dpg.font_registry():
+        with dpg.font('NotoSansSC-Regular.otf', size, default_font=True) as simple_chinese_font:
+            dpg.add_font_range_hint(dpg.mvFontRangeHint_Chinese_Full)
         if debug_mode:
             for file_name in os.listdir(os.path.curdir):
                 if re.match('.*\.(ttf|TTF|otf)', file_name):
                     print(file_name)
-                    with ui.font(file_name, size):
-                        ui.add_font_range_hint(ui.mvFontRangeHint_Default)
-                        ui.add_font_range_hint(ui.mvFontRangeHint_Chinese_Simplified_Common)
-    ui.bind_font(simple_chinese_font)
+                    with dpg.font(file_name, size):
+                        dpg.add_font_range_hint(dpg.mvFontRangeHint_Chinese_Full)
+    dpg.bind_font(simple_chinese_font)
+
+def teleport_cb(sender, data):
+    print(sender.id)
 
 def load_list_view(file_name : str):
-    with open(file_name, 'r', encoding='utf-8') as f:
-        lines = f.readlines()
+    with open(file_name, 'r', encoding='utf-8')as file:
+        lines = file.readlines()
         if len(lines)%4 != 0:
             print(f"{file_name} lines count wrong")
             return
         for i in range(int(len(lines)/4)):
-            with ui.table_row():
+            with dpg.table_row():
                 for j in range(4):
-                    # ui.add_text(lines[i*4 + j])
-                    ui.add_text('测试  厄')
+                    if j > 0:
+                        f = float(lines[i*4 + j])
+                        dpg.add_text("%0.3f" % f)
+                    else:
+                        # dpg.add_text(lines[i*4 + j])
+                        btn = dpg.add_button(label=lines[i*4 + j], width=300, callback=lambda ctrl:teleport_cb(ctrl))
+
+                    # dpg.add_text('测试  厄')
 
 if __name__ == "__main__":
-    ui.create_context()
+    dpg.create_context()
 
     debug_mode = True
-    debug_mode = False
+    # debug_mode = False
 
-
+    themes = []
     set_font(debug_mode)
-    width = set_theme(debug_mode)
+    width = show_debug_editor(debug_mode)
+    set_theme(themes)
+    border_theme = themes[1]
 
-    ui.create_viewport(title="Teleport", width=width, height=900, decorated=True)
-    ui.setup_dearpygui()
+    dpg.create_viewport(title="Teleport", width=width, height=900, decorated=True)
+    dpg.setup_dearpygui()
 
-    with ui.window(label="Example Window", tag="Primary Window"):
-        with ui.tab_bar():
-            with ui.tab(label="functional"):
-                with ui.child_window(width=380, height=500):
-                    ui.add_text("list_view")
-                    with ui.table(header_row=True, row_background=True,
+    with dpg.window(label="Example Window", tag="Primary Window"):
+        with dpg.tab_bar():
+            with dpg.tab(label="functional"):
+                with dpg.child_window(width=600, height=500):
+                    dpg.add_text("list_view")
+                    with dpg.table(header_row=True, row_background=True, resizable=True,
                                    borders_innerH=True, borders_outerH=True, borders_innerV=True,
-                                   borders_outerV=True, delay_search=True) as table_id:
-                        ui.add_table_column(label="描述")
-                        ui.add_table_column(label="X")
-                        ui.add_table_column(label="Y")
-                        ui.add_table_column(label="Z")
+                                   borders_outerV=True, delay_search=True, policy=dpg.mvTable_SizingFixedFit,
+                                   no_pad_innerX=True
+                                   ) as table_id:
+                        dpg.add_table_column(label="描述")
+                        dpg.add_table_column(label="X")
+                        dpg.add_table_column(label="Y")
+                        dpg.add_table_column(label="Z")
                         load_list_view('favlist.fav')
-                        # for i in range(20):
-                        #     with ui.table_row():
-                        #         for j in range(4):
-                        #             ui.add_text(f"行{i} 列{j}")
-                        # with ui.table_row():
-                        #     ui.add_text("行0 列0")
 
-                with ui.child_window(width=380, height=100):
-                    ui.add_text("button region")
-                    ui.add_checkbox(label="sync_teleport")
-                with ui.child_window(width=380, height=200):
-                    ui.add_text("logs:")
-            with ui.tab(label="nodes"):
-                with ui.child_window(width=380, height=800):
-                    ui.add_text("nodes")
-            with ui.tab(label="config"):
-                with ui.child_window(width=380, height=800):
-                    ui.add_text("config")
+                with dpg.child_window(width=600, height=100):
+                    dpg.add_text("button region")
+                    sync_teleport = dpg.add_checkbox(label="sync_teleport")
+                    dpg.bind_item_theme(sync_teleport, border_theme)
+                with dpg.child_window(width=600, height=200):
+                    dpg.add_text("logs:")
+            with dpg.tab(label="nodes"):
+                with dpg.child_window(width=600, height=800):
+                    dpg.add_text("nodes")
+            with dpg.tab(label="config"):
+                with dpg.child_window(width=600, height=800):
+                    dpg.add_text("config")
 
-    ui.show_viewport()
-    ui.set_primary_window("Primary Window", True)
-    ui.start_dearpygui()
-    ui.destroy_context()
+    dpg.show_viewport()
+    dpg.set_primary_window("Primary Window", True)
+    dpg.start_dearpygui()
+    dpg.destroy_context()
